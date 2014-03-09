1f495b1092ff5d9beacf0761b4398c683767573f
Cloud1212 <cloud12128@gmail.com>
2014-03-09 15:55:38 -0600

    accesorios
    accesorios

---
+++ baja_accesorios.aspx
@@ -0,0 +1,58 @@
+﻿<%@ Page Title="" Language="VB" MasterPageFile="~/GICR.master" AutoEventWireup="false" CodeFile="baja_accesorios.aspx.vb" Inherits="baja_accesorios" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+    <style type="text/css">
+        .auto-style1 {
+            width: 170px;
+        }
+        .auto-style2 {
+            width: 428px;
+        }
+    </style>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <table style="width:100%;">
+        <tr>
+            <td class="auto-style1">
+                <asp:Label ID="lblcveAccesorio" runat="server" Text="Clave Accesorio"></asp:Label>
+            </td>
+            <td class="auto-style2">
+                <asp:TextBox ID="txtCveAccesorio" runat="server" Width="180px" MaxLength="3"></asp:TextBox>
+            </td>
+            <td>&nbsp;</td>
+        </tr>
+        <tr>
+            <td class="auto-style1">
+                <asp:Label ID="lblAccesorioLike" runat="server" Text="Palabra Clave"></asp:Label>
+            </td>
+            <td class="auto-style2">
+                <asp:TextBox ID="txtPalabraClave" runat="server" Width="180px" MaxLength="200"></asp:TextBox>
+            </td>
+            <td>&nbsp;</td>
+        </tr>
+        <tr>
+            <td class="auto-style1">
+                <asp:Button ID="btnBuscarSeleccionado" runat="server" Text="Buscar por seleccion" Width="191px" />
+            </td>
+            <td class="auto-style2">
+                <asp:Button ID="btnBuscarTodos" runat="server" Text="Buscar Todos" Width="191px" />
+            </td>
+            <td>&nbsp;</td>
+        </tr>
+        <tr>
+            <td class="auto-style1">
+                &nbsp;</td>
+            <td class="auto-style2">
+                <asp:GridView ID="grdbajaAccesorios" runat="server" DataKeyNames ="cve_accesorio" AutoGenerateColumns="False">
+                    <Columns>
+                        <asp:BoundField DataField="cve_accesorio" HeaderText="Clave Accesorio" />
+                        <asp:BoundField DataField="descr_accesorio" HeaderText="Descripcion Accesorio" />
+                        <asp:CommandField ShowDeleteButton="True" DeleteText="Borrar Registro" />
+                    </Columns>
+                </asp:GridView>
+            </td>
+            <td>&nbsp;</td>
+        </tr>
+    </table>
+</asp:Content>
+
---
+++ baja_accesorios.aspx.vb
@@ -0,0 +1,73 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+Partial Class baja_accesorios
+    Inherits System.Web.UI.Page
+
+    Protected Sub btnBuscarSeleccionado_Click(sender As Object, e As EventArgs) Handles btnBuscarSeleccionado.Click
+
+        Dim cl As New Chevrolet
+        Dim ds As DataSet
+        Dim query As String
+        Dim clave, palabra As String
+
+        clave = txtCveAccesorio.Text
+        palabra = txtPalabraClave.Text
+        If clave.Length < 3 Then
+            Response.Write("<script languaje=javascript> alert('La clave del accesorio debe de contener 3 caracteres');</script>")
+            GoTo end1
+        End If
+
+        If clave = "" And palabra = "" Then
+            Response.Write("<script languaje=javascript> alert('Debe de ingresar al menos uno de los 2 campos');</script>")
+            GoTo end1
+        ElseIf clave.Length > 0 Then
+            query = "select * from accesorios where cve_accesorio = '{0}'"
+            query = String.Format(query, clave)
+            ds = cl.retrieve(query, CommandType.Text)
+            Me.grdbajaAccesorios.DataSource = ds
+            Me.grdbajaAccesorios.DataBind()
+        ElseIf palabra.Length > 0 Then
+            query = "select * from accesorios where descr_accesorio like '{0}' + '%'"
+            query = String.Format(query, palabra)
+            ds = cl.retrieve(query, CommandType.Text)
+            Me.grdbajaAccesorios.DataSource = ds
+            Me.grdbajaAccesorios.DataBind()
+        End If
+
+end1:
+        Response.Redirect(Request.Url.AbsoluteUri)
+
+    End Sub
+    Protected Sub btnBuscarTodos_Click(sender As Object, e As EventArgs) Handles btnBuscarTodos.Click
+        Dim cl As New Chevrolet
+        Dim ds As DataSet
+        Dim query As String
+
+        query = "select * from accesorios"
+        ds = cl.retrieve(query, CommandType.Text)
+        Me.grdbajaAccesorios.DataSource = ds
+        Me.grdbajaAccesorios.DataBind()
+
+    End Sub
+    Protected Sub grdbajaAccesoriosRowDeleting(sender As Object, e As GridViewDeleteEventArgs) Handles grdbajaAccesorios.RowDeleting
+
+        Dim cl As New Chevrolet
+        Dim ds As DataSet
+        Dim query As String
+        Dim reng As Integer
+        Dim llave As String
+
+
+
+        reng = e.RowIndex
+        llave = (grdbajaAccesorios.DataKeys(reng).Values)(0).ToString
+        query = String.Format("delete accesorios where cve_accesorio= '{0}'", llave)
+        ds = cl.retrieve(query, CommandType.Text)
+
+        MsgBox("El registro " + llave + " ha sido eliminado exitosamente del sistema", MsgBoxStyle.Information, "Mensaje del sistema")
+
+        Response.Redirect(Request.Url.AbsoluteUri)
+
+
+    End Sub
+End Class
