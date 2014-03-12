33d8be674956d93f03daacecd0720550af79810f
Cloud1212 <cloud12128@gmail.com>
2014-03-11 18:20:29 -0600

    Agregado el cerrar sesion
    Se implemento en el menu la opcion para cerrar sesion, en la parte donde
    biene el nombre de quien esta logeado

---
+++ logout.aspx.vb
@@ -0,0 +1,10 @@
+﻿
+Partial Class logout
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Session.Clear()
+        Session.Abandon()
+        Response.Redirect("~/logon.aspx")
+    End Sub
+End Class
