02c41b0b7e2b305e8b05b7c98e76690ba420586e
Cloud1212 <cloud12128@gmail.com>
2014-04-09 17:43:20 -0500

    Menu Estetico
    + Cambio en la forma del menu
    + Los permisos del menu van de la mano a los establecidos en el
    GICR.master.vb

--- GICR.master.vb
+++ GICR.master.vb
@@ -2,6 +2,8 @@
 Partial Class GICR
     Inherits System.Web.UI.MasterPage
 
+    Public htmlMenu As String
+    Public htmlUser As String
     Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
         'Comprueba que exista la session
         If Session("Activo_GICR") <> "" Then
@@ -55,34 +57,20 @@ Partial Class GICR
             'Comprueba que exista el tipo de usuario
             If Not permisos.ContainsKey(reg("perfil_Usuario").ToString.Trim.ToLower) Then termina_session()
 
-            'Comprueba que exista tenga permiso de var la paguina
+            'Comprueba que exista tenga permiso de ver la paguina
             If Not permisos(reg("perfil_Usuario").ToString).Contains(url) Then termina_session()
 
-            Select Case reg("perfil_Usuario").ToString
-                Case "supervisor"
-                    Menu1.Items.Clear()
-                    Menu1.Items.Add(New MenuItem(text:="INICIO", value:="INICIO", imageUrl:="", navigateUrl:="~/Inicio.aspx"))
-                    Menu1.Items.Add(New MenuItem(text:="Reportes", value:="Reportes"))
-                    Menu1.Items(1).ChildItems.Add(New MenuItem(text:="Reporte de antiguedad", value:="Futuros Embarques", imageUrl:="", navigateUrl:="~/RepAnti.aspx"))
-                    Menu1.Items.Add(New MenuItem(text:="Consultas", value:="Consultas"))
-                    Menu1.Items(2).ChildItems.Add(New MenuItem(text:="Consulta de producto", value:="Consulta de producto", imageUrl:="", navigateUrl:="~/ConsultaProd.aspx"))
-                    Menu1.Items(2).ChildItems.Add(New MenuItem(text:="Consulta existencias", value:="Consulta existencias", imageUrl:="", navigateUrl:="~/ConsultaExist.aspx"))
-                Case "ventas"
-                    Menu1.Items.Clear()
-                    Menu1.Items.Add(New MenuItem(text:="INICIO", value:="INICIO", imageUrl:="", navigateUrl:="~/Inicio.aspx"))
-                    Menu1.Items.Add(New MenuItem(text:="Apartados", value:="Apartados"))
-                    Menu1.Items.Add(New MenuItem(text:="Reportes", value:="Reportes"))
-                    Menu1.Items(2).ChildItems.Add(New MenuItem(text:="Reporte de antiguedad", value:="Futuros Embarques", imageUrl:="", navigateUrl:="~/RepAnti.aspx"))
-                    Menu1.Items.Add(New MenuItem(text:="Consultas", value:="Consultas"))
-                    Menu1.Items(3).ChildItems.Add(New MenuItem(text:="Consulta de producto", value:="Consulta de producto", imageUrl:="", navigateUrl:="~/ConsultaProd.aspx"))
-                    Menu1.Items(3).ChildItems.Add(New MenuItem(text:="Consulta existencias", value:="Consulta existencias", imageUrl:="", navigateUrl:="~/ConsultaExist.aspx"))
-            End Select
-
-            If (Menu1.Items(Menu1.Items.Count - 1).Text <> reg("nom_Usuario").ToString.Trim) Then
-                Menu1.Items.Add(New MenuItem(reg("nom_Usuario").ToString.Trim))
-                Menu1.Items(Menu1.Items.Count - 1).ChildItems.Add(New MenuItem(text:="Cerrar sesion", value:="Cerrar sesion", imageUrl:="", navigateUrl:="~/logout.aspx"))
-            End If
-
+            htmlMenu = mnuToHTML(Menu1, permisos(reg("perfil_Usuario").ToString))
+            Menu1.Items.Clear()
+            htmlUser = "<li class=""dropdown"">" + Chr(10) + _
+                        "<a href=""#"" class=""dropdown-toggle"" data-toggle=""dropdown"">" + _
+                        "<span class=""glyphicon glyphicon-user""></span> " + _
+                        Regex.Replace(reg("nom_Usuario").ToString.Trim, "\b\w", AddressOf upperCase) + _
+                        " <b class=""caret""></b></a>" + Chr(10) + _
+                        "<ul class=""dropdown-menu"">" + Chr(10) + _
+                        "<li><a href=""#""><span class=""glyphicon glyphicon-off""></span> Cerrar Sesion</a></li>" + Chr(10) + _
+                        "</ul>" + Chr(10) + _
+                        "</li>"
         Else
             termina_session()
         End If
@@ -94,48 +82,50 @@ Partial Class GICR
         Session.Abandon()
         Response.Redirect("~/logon.aspx")
     End Sub
-    Private Function muestra(ByVal menuBase As Menu, ByRef permite As List(Of String)) As Menu
-        Dim menuRetorno As New Menu
-        Dim menu As New MenuItem
-        Dim cant As Integer = menuBase.Items.Count - 1
-        Dim a As Integer = 0
-        Dim url As String = ""
-
-        For i As Integer = 0 To cant
-            menu = menuBase.Items(i - a)
-            If menu.ChildItems.Count = 0 Then
-                url = Regex.Replace(menu.NavigateUrl, "(([^\n]+?)(/|\\))?([^\\/]+\.aspx)([^\n]*)", "$4").ToString.ToLower
-                If permite.Contains(url) Then
-                    a += 1
-                    If menu.ChildItems.Count > 0 Then menuRetorno.Items.Add(menu)
-
-                End If
-            Else
-                menuRetorno.Items.Add(muestra(menu, permite))
-            End If
-        Next i
-        Return menuRetorno
+    Function upperCase(m As Match) As String
+        Return m.ToString.ToUpper.Trim
     End Function
-    Private Function muestra(ByVal menuBase As MenuItem, ByRef permite As List(Of String)) As MenuItem
-        Dim menuRetorno As New MenuItem(menuBase.Text)
-        Dim menu As New MenuItem
-        Dim cant As Integer = menuBase.ChildItems.Count - 1
-        Dim a As Integer = 0
-        Dim url As String = ""
-
-        For i As Integer = 0 To cant
-            menu = menuBase.ChildItems(i - a)
-            If menu.ChildItems.Count = 0 Then
-                url = Regex.Replace(menu.NavigateUrl, "(([^\n]+?)(/|\\))?([^\\/]+\.aspx)([^\n]*)", "$4").ToString.ToLower
-                If permite.Contains(url) Then
-                    a += 1
-                    If menu.ChildItems.Count > 0 Then menuRetorno.ChildItems.Add(menu)
+    Function mnuToHTML(ByRef menu As Menu, ByRef allow As List(Of String)) As String
+        Dim html As String = ""
+        For Each subMenu As MenuItem In menu.Items
+            If subMenu.ChildItems.Count Then
+                Dim hasChild As String = mnuToHTML(subMenu, allow)
+                If hasChild.Length Then
+                    html += String.Format(" <li class=""dropdown"">" + Chr(10) + _
+                        "<a href=""#"" class=""dropdown-toggle"" data-toggle=""dropdown"">" + subMenu.Text + "<b class=""caret""></b></a>" + Chr(10) + _
+                        "<ul class=""dropdown-menu"">" + Chr(10) + _
+                        "{0}" + Chr(10) + _
+                        "</ul>" + Chr(10) + _
+                        "</li>", hasChild)
                 End If
             Else
-                menuRetorno.ChildItems.Add(muestra(menu, permite))
+                If allow.Contains(Regex.Replace(subMenu.NavigateUrl, "(([^\n]+?)(/|\\))?([^\\/]+\.aspx)([^\n]*)", "$4").ToLower) Then
+                    html += "<li><a href=""" & subMenu.NavigateUrl.Replace("~", ".") & """>" & subMenu.Text & "</a></li>" & Chr(10)
+                End If
             End If
         Next
-        Return menuRetorno
+        Return html
     End Function
-End Class
+    Function mnuToHTML(ByRef menu As MenuItem, ByRef allow As List(Of String)) As String
+        Dim html As String = ""
+        For Each subMenu As MenuItem In menu.ChildItems
+            If subMenu.ChildItems.Count Then
+                Dim hasChild As String = mnuToHTML(subMenu, allow)
+                If hasChild.Length Then
+                    html += String.Format(" <li class=""dropdown"">" + Chr(10) + _
+                        "<a href=""#"" class=""dropdown-toggle"" data-toggle=""dropdown"">" + subMenu.Text + "<b class=""caret""></b></a>" + Chr(10) + _
+                        "<ul class=""dropdown-menu"">" + Chr(10) + _
+                        "{0}" + Chr(10) + _
+                        "</ul>" + Chr(10) + _
+                        "</li>", hasChild)
+                End If
+            Else
+                If allow.Contains(Regex.Replace(subMenu.NavigateUrl, "(([^\n]+?)(/|\\))?([^\\/]+\.aspx)([^\n]*)", "$4").ToLower) Then
+                    html += "<li><a href=""" & subMenu.NavigateUrl.Replace("~", ".") & """>" & subMenu.Text & "</a></li>" & Chr(10)
+                End If
+            End If
+        Next
+        Return html
+    End Function
 
+End Class
--- js/helper.js
+++ js/helper.js
@@ -1,9 +1,19 @@
 // Usado solo para la mostrar mostrar la ayuda
-$('[data-toggle=popover]').popover({trigger: 'click'});
+$('[data-toggle=popover]').popover({trigger: 'click',html:true});
 $('[data-toggle=popover]').on('show.bs.popover', function (event) {
     $('[data-toggle=popover]').each(function () {
         if (event.target != this) {
             $(this).popover('hide');
         }
     });
+});
+$('ul.nav > li.dropdown > ul').on('shown.bs.dropdown', function () {
+    var that = this;
+    var nav = (that.nodeName == nav || $(that).hasClass('nav'));
+    var i = 0;
+    while (i++ < 2 && !nav) {
+        that = that.parentNode;
+        nav = (that.nodeName == nav || $(that).hasClass('nav'));
+        if ($(that).hasClass('dropdown')) $(that).removeClass("open").addClass("open");
+    }
 });
